011d8c7d221e470cab67afd27e176b92aff1b799
Bogdanov Ilya <bogdanov@comepay.ru>
2018-05-19 14:44:14 +0300

    Изменил в форме сборку данных с form

--- client/js/index.js
+++ client/js/index.js
@@ -8,7 +8,9 @@ $(document).ready(function () {
         let inp1 = $('#inp1').val(),
             inp2 = $('#inp2').val();
 
-        if (inp1.length < 2 || inp2.length < 2) return false; // Если данные пустые
+        if (inp1.length < 2 || inp2.length < 2) {
+            return false; // Если данные пустые
+        }
 
         let form = $('.form'),
             data = form.serialize();
@@ -16,15 +18,15 @@ $(document).ready(function () {
         console.log('data', data);
         
         /* Запишем данные в localstorage */
-        localStorage.setItem('inp1', inp1);
-        localStorage.setItem('inp2', inp2);
+        localStorage.setItem('name', inp1);
+        localStorage.setItem('family', inp2);
         
         // Чистим форму
         form[0].reset();
         
         /* Вытащим данные из localstorage */
-        let name = localStorage.getItem('inp1'), 
-            family = localStorage.getItem('inp2');
+        let name = localStorage.getItem('name'), 
+            family = localStorage.getItem('family');
         
         $('.name').html(name);
         $('.family').html(family);

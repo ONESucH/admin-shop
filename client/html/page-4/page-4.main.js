0fb95eb9362b8576b6bb58527f1716985133ba74
Bogdanov Ilya <bogdanov@comepay.ru>
2018-07-24 16:39:41 +0300

    Правки в чате

--- client/html/page-4/page-4.main.js
+++ client/html/page-4/page-4.main.js
@@ -1,4 +1,28 @@
 'use strict';
 
+let findUser = localStorage.getItem('nick');
+
 if (!findUser) window.location.href = '/';
 
+/* Чат игроков */
+$('form').submit((e) => {
+    e.preventDefault();
+    
+    let message = $('#user-message').val();
+    
+    if (message === '') return false;
+
+    $('.all-message-user').prepend(
+        '<div class="message-user">\n' +
+        '   <p class="d-inline-block w-100">\n'+
+        '   <img class="rounded-circle user d-inline-block w-auto m-2" src="../../img/user-undefined.jpg">'+findUser+': '+message+'</p>' +
+        '</div>'
+    );
+    
+    // Если много объектов удалем, чтобы не грузить ПК
+    if ($('.message-user').length > 20) {
+        $('.message-user')[20].remove();
+    }
+    
+    $('#user-message').val('');
+});
--- client/vendor/jquery/src/event.js
+++ client/vendor/jquery/src/event.js
@@ -681,11 +681,7 @@ jQuery.each( {
 		bindType: fix,
 
 		handle: function( event ) {
-			var ret,
-				target = this,
-				related = event.relatedTarget,
-				handleObj = event.handleObj;
-
+            submit
 			// For mouseenter/leave call the handler if related is outside the target.
 			// NB: No relatedTarget if the mouse left/entered the browser window
 			if ( !related || ( related !== target && !jQuery.contains( target, related ) ) ) {
